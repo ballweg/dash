6a452e82a43d2aaace83517828fb827d3dd803b7
Jeff Ballweg <ballweg@users.noreply.github.com>
2017-01-30 10:57:47 -0600

    add short month, day of week to clock

--- main.js
+++ main.js
@@ -6,13 +6,17 @@
 
 function updateClock() {
   var now = new Date(), // current date
-    months = ['January', 'February', 'March', 'April', 'May', 'June',
-              'July', 'August', 'September', 'October', 'November', 'December'];
+    months   = ['January', 'February', 'March', 'April', 'May', 'June',
+                'July', 'August', 'September', 'October', 'November', 'December'];
+    months_s = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+    days     = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
     time = now.getHours() + ':' + (now.getMinutes()<10?'0':'' + now.getMinutes()),
     // build month string
-    date = [months[now.getMonth()],
-            now.getDate() +',',
-            now.getFullYear()].join(' ');
+    date = [days[now.getDay()] + ',',
+            months_s[now.getMonth()],
+            now.getDate(),
+           ].join(' ');
 
   document.querySelector(".time").innerHTML = time;
   document.querySelector(".date").innerHTML = date;
